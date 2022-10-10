7589daf6253ccdf7f82499dba6857d9c6e3e66fe
Vasyl Horaichuk <vg.createam@gmail.com>
2022-10-10 20:38:22 +0300

    Тест

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+"use strict"
+
+console.log(document);
+
+const listCategoryWithId = document.querySelector('#categories');
+console.log(listCategoryWithId);
+
+for (const elem of categories) { 
+
+}
+
+array.map((element, index, array) => {
+  
+});
