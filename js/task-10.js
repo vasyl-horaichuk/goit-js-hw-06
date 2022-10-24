ff48f212356cf239c767da107cfd7205d2aa81fa
Vasyl Horaichuk <vg.createam@gmail.com>
2022-10-24 15:16:30 +0300

    Виконав всі 10 дз

--- js/task-10.js
+++ js/task-10.js
@@ -1,33 +1,37 @@
 'use strict'
 
+const inputRef = document.querySelector('[type="number"]');
+const buttonCreateRef = document.querySelector('[data-create]');
+const buttonDestroyRef = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const createBoxes = (amount) => { 
+  const elementsToAdd = [];
+  for (let i = 0; i < amount; i+=1 ) { 
+    const divRef = document.createElement('div')
+    divRef.style.height = `${30 + 10 * i}px`
+    divRef.style.width = `${30 + 10 * i}px`
+    divRef.style.background = getRandomHexColor()
+    elementsToAdd.push(divRef)
+  }
+  return elementsToAdd;
+} 
 
+const destroyBoxes = () => { 
+  boxesRef.innerHTML = '';
+}
 
+buttonCreateRef.addEventListener('click', () => {
+	let boxesToAdd = createBoxes(inputRef.value)
+	boxesRef.append(...boxesToAdd)
+})
 
-// Напиши скрипт створення і очищення колекції елементів.Користувач вводить кількість елементів в input і натискає кнопку Створити,
-//   після чого рендериться колекція.Натисненням на кнопку Очистити, колекція елементів очищається.
+console.log(inputRef.value);
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
-// Створи функцію createBoxes(amount), яка приймає один параметр - число.Функція створює стільки < div >, скільки вказано в amount і додає їх
-//  у div#boxes.
-
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+buttonDestroyRef.addEventListener('click', () => {
+	destroyBoxes.call()
+})
